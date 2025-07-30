90ebec1e0f1916de2bdbaba24fbb80fd09613ce8
flnames <getitoverrr@gmail.com>
2025-07-30 10:25:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 from flask import Flask, jsonify
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 app = Flask(__name__)
 
-# Hardcoded start date (UTC)
-START_DATE = datetime(2023, 1, 1, 0, 0, 0)
-INTERVAL_MINUTES = 5
-MAX_MONTHS = 1200  # Cap at 100 years
+# Hardcoded reference date
+BASE_DATE = datetime(2023, 1, 1)
+
+# Track when the service was started
+SERVICE_START_TIME = datetime.utcnow()
+
+INTERVAL = timedelta(minutes=5)
 
 @app.route('/currdate', methods=['GET'])
-def get_currdate():
+def currdate():
     now = datetime.utcnow()
-    elapsed_minutes = (now - START_DATE).total_seconds() // 60
-    intervals_passed = int(elapsed_minutes // INTERVAL_MINUTES)
+    elapsed_time = now - SERVICE_START_TIME
 
-    # Cap the month increment
-    if intervals_passed > MAX_MONTHS:
-        intervals_passed = MAX_MONTHS
+    # Number of 5-minute intervals since service started
+    intervals_passed = int(elapsed_time.total_seconds() // INTERVAL.total_seconds())
 
-    current_date = START_DATE + relativedelta(months=intervals_passed)
+    # Add one month per interval to the BASE_DATE
+    current_date = BASE_DATE + relativedelta(months=intervals_passed)
+
     return jsonify({'currentDate': current_date.strftime('%Y-%m-%d')})
+
