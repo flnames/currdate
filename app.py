9061856db5484fce8371f1b232c6eacc99b55b6d
flnames <getitoverrr@gmail.com>
2025-07-30 13:32:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,8 @@ from dateutil.relativedelta import relativedelta
 
 app = Flask(__name__)
 
-# Hardcoded reference date
 BASE_DATE = datetime(2023, 1, 1)
-
-# Track when the service was started
 SERVICE_START_TIME = datetime.utcnow()
-
 INTERVAL = timedelta(minutes=5)
 
 @app.route('/currdate', methods=['GET'])
@@ -17,11 +13,16 @@ def currdate():
     now = datetime.utcnow()
     elapsed_time = now - SERVICE_START_TIME
 
-    # Number of 5-minute intervals since service started
     intervals_passed = int(elapsed_time.total_seconds() // INTERVAL.total_seconds())
-
-    # Add one month per interval to the BASE_DATE
     current_date = BASE_DATE + relativedelta(months=intervals_passed)
 
-    return jsonify({'currentDate': current_date.strftime('%Y-%m-%d'),'time':elapsed_time % INTERVAL})
+    # Calculate time left for next interval
+    remaining = INTERVAL - (elapsed_time % INTERVAL)
+    remaining_seconds = int(remaining.total_seconds())
+
+    return jsonify({
+        'currentDate': current_date.strftime('%Y-%m-%d'),
+        'time_until_next_update_seconds': remaining_seconds
+    })
+
 
