6b7ddeb4910e6ab1b0ce9d04dd9faca62df56c6e
flnames <getitoverrr@gmail.com>
2025-07-31 11:30:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
@@ -23,9 +23,30 @@ def next_date():
         'newDate': current_fake_date.strftime('%Y-%m-%d')
     })
 
+@app.route('/prev', methods=['POST'])
+def prev_date():
+    global current_fake_date
+    current_fake_date -= relativedelta(months=1)
+    return jsonify({
+        'message': 'Date moved back by 1 month',
+        'newDate': current_fake_date.strftime('%Y-%m-%d')
+    })
+
+@app.route('/date', methods=['POST'])
+def set_date():
+    global current_fake_date
+    date_str = request.args.get('date')
+    if not date_str:
+        return jsonify({'error': 'Missing "date" query parameter'}), 400
+    try:
+        current_fake_date = datetime.strptime(date_str, '%Y-%m-%d')
+        return jsonify({
+            'message': f'Date set to {date_str}',
+            'newDate': current_fake_date.strftime('%Y-%m-%d')
+        })
+    except ValueError:
+        return jsonify({'error': 'Invalid date format. Use YYYY-MM-DD.'}), 400
+
 @app.route('/')
 def health():
     return "Fake Date Service Running"
-
-
-
